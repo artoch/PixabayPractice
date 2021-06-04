af47c4ed1d517f8828b32e63a2a22cc640a2b351
Artoch <artochq@gmail.com>
2021-06-04 18:16:34 -0400

    fix something

--- app/src/main/java/com/atsapp/pixabaytestapp/utils/Const.kt
+++ app/src/main/java/com/atsapp/pixabaytestapp/utils/Const.kt
@@ -5,8 +5,6 @@ class Const {
 
     companion object {
         val API_URL = "https://pixabay.com/api/"
-        val API_KEY = "13119377-fc7e10c6305a7de49da6ecb25"
-        const val FIRTS_PAGE = 1
-        const val POST_PER_PAGE = 10
+        val API_KEY = "YOUR-APIKEY"
     }
 }
