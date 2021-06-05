2bc39c2e8db96f714eadf9c56c1c4cb517e99302
Artoch <artochq@gmail.com>
2021-06-04 21:52:50 -0400

    optimization http

--- app/src/main/java/com/atsapp/pixabaytestapp/BaseApp.kt
+++ app/src/main/java/com/atsapp/pixabaytestapp/BaseApp.kt
@@ -1,7 +1,19 @@
 package com.atsapp.pixabaytestapp
 
 import android.app.Application
+import android.content.Context
+import com.atsapp.pixabaytestapp.utils.SessionManager
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class BaseApp : Application()
+class BaseApp : Application(){
+    companion object {
+        lateinit var appContext: Context
+        val sessionManager by lazy { SessionManager(appContext) }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        appContext = this
+    }
+}
